76bef732a34bc0fc52cab648ebc1d85863919438
Nathaniel <nstanton1@sheffield.ac.uk>
2024-06-17 00:20:33 +0100

    Made some navbar design tweaks but no perfomance changes,

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,7 +4,7 @@ import { Link } from 'gatsby'
 
 const NavbarLink = ({ title, to }) => (
     <Link
-      className="w-full lg:w-auto text-lg lg:text-base text-center lg:mx-2 flex-shrink-0 flex-grow-0 border-b-2 hover:font-medium hover:border-b-4 duration:300"
+      className="w-full lg:w-auto text-lg lg:text-base text-center my-1 lg:my-0 lg:mx-2 lg:px-1 flex-shrink-0 flex-grow-0 border-b-2 hover:font-medium hover:bg-gray-600/15 duration:300"
       to={to}
     >
       {title}
@@ -18,7 +18,7 @@ NavbarLink.propTypes = {
 
 const NavbarToggle = ({ isOpen, callback }) => (
     <button
-        className="flex-shrink-0 flex-grow-0 mr-2 border-2 p-2 border-gray-800 text-gray-200 lg:hidden"
+        className="flex-shrink-0 flex-grow-0 mr-4 p-2 text-gray-200 lg:hidden"
         onClick={callback}
     >
         <div className={`${isOpen ? "hidden" : "block"}`}>Menu</div>
@@ -57,15 +57,15 @@ export default class Navbar extends React.Component {
         return (
         <div className="self-start lg:self-end">
             <nav
-            className="flex flex-wrap lg:flex-row lg:max-w-6xl flex-shrink-0 flex-grow text-white mx-auto mb-1 items-end justify-between border-2 border-black"
+            className="flex flex-wrap lg:flex-row lg:max-w-6xl flex-shrink-0 flex-grow text-white mx-auto mb-1 items-end justify-between"
             role="navigation"
             aria-label="main-navigation"
             >
-                <h1 className="w-auto px-8 py-2 text-xl max-w-fit md:text-3xl lg:text-5xl text-white font-medium border-2 border-black">Wanderers Nook</h1>
+                <h1 className="w-auto px-8 py-2 text-xl max-w-fit md:text-3xl lg:text-5xl text-white font-medium">Wanderers Nook</h1>
                 <div className="flex">
                     <NavbarToggle isOpen={this.state.menuOpen} callback={this.toggle} />
                 </div>
-                <div className={'flex-shrink-0 lg:py-2 w-full md:w-auto flex-wrap items-center border-2 border-black ' + this.state.menuOpenClassName}>
+                <div className={'flex-shrink-0 lg:py-2 w-full md:w-auto flex-wrap items-center ' + this.state.menuOpenClassName}>
                     <NavbarLink to={'/'} title={'Home'} />
                     <NavbarLink to={'/'} title={'Routes'} />
                     <NavbarLink to={'/'} title={'Adventures'} />
