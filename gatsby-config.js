ae0c9911067407a08beda5705c61b9e40b63e55f
Nathaniel <nstanton1@sheffield.ac.uk>
2024-06-20 15:37:49 +0100

    Updated the SEO component to set the SEO advised by Dan. Default values established and isRequired set for required Props.

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,10 @@ module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
     title: 'Wanderers Nook',
+    author: 'Nathaniel J Stanton',
+    description: 'Discover adventure in and around Manchester. Newcomers and veterans alike can find guides on exploring nature, history, and the great outdoors—all by public transport.',
+    image: './src/images.Me.pg', 
+    siteSearch: 'nositelinkssearchbox',
   },
   plugins: [
     "gatsby-plugin-image",
--- src/components/seo.js
+++ src/components/seo.js
@@ -1,20 +1,63 @@
 import * as React from 'react'
 import { graphql, useStaticQuery } from 'gatsby'
 
-const Seo = ({ title }) => {
+const Seo = ({ pageTitle, pageDescription, pageURL, pageImage }) => {
   const data = useStaticQuery(graphql`
     query {
       site {
         siteMetadata {
           title
+          description
+          author
+          image
+          siteSearch
+          siteURL
         }
       }
     }
   `)
 
+  const metaTitle = pageTitle ? `${pageTitle} | ${data.site.siteMetadata.title}` : `${data.site.siteMetadata.title}`
+  const metaDescription = pageDescription ? `${pageDescription}` : `${data.site.siteMetadata.description}`
+  const metaImageAbsoluteURL = pageImage ? `${data.site.siteMetadata.siteUrl}/${pageImage}` : `${data.site.siteMetadata.siteUrl}/${data.site.siteMetadata.image}`
+  const metaPageUrl = `${data.site.siteMetadata.siteURL}${pageURL}`
+
   return (
-    <title>{title} | {data.site.siteMetadata.title}</title>
+    <>
+      {/* General tags for all */}
+      <meta charset="UTF-8" />
+      <meta name="robots" content="index, follow" />
+      <meta name="author" content={data.site.siteMetadata.author}/>
+      <meta http-equiv="Content-Language" content="en"/> 
+      <meta name="google" content={data.site.siteMetadata.siteSearch}/>
+
+      {/* Page specidic tags */}
+      <title>{metaTitle}</title>
+      <meta name="description" content={metaDescription} />
+      {/* Canonical link is just the page's URL. Must be injected on page creation */}
+      <link rel="canonical" href={metaPageUrl} />    
+
+      {/* Open Graph Tags */}
+      <meta property="og:title" content={metaTitle} />
+      <meta property="og:description" content={metaDescription} />
+      {metaImageAbsoluteURL && <meta property="og:image" content={metaImageAbsoluteURL} />}
+      <meta property="og:url" content={pageURL} />
+
+      {/* Twitter Card Tags */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={metaTitle}/>
+      <meta name="twitter:description" content={metaDescription}/>
+      {metaImageAbsoluteURL && <meta name="twitter:image" content={metaImage}/>}
+
+    </>
   )
+}
+
+Seo.propTypes = {
+  pageTitle: PropTypes.string.isRequired,
+  pageDescription: PropTypes.string,
+  pageImage: PropTypes.string,
+  pageURL: PropTypes.string.isRequired,
 }
 
 export default Seo
