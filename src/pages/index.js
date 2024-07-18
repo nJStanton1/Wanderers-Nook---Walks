4eec5d80601f30b9cdd9717c071ca19d5f9135fa
Nathaniel <nstanton1@sheffield.ac.uk>
2024-07-18 17:38:59 +0100

    Mobile and iPad responsiveness for home page done.

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,27 +16,27 @@ const IndexPage = () => {
           src="../images/Me.jpg"
           object-position="50% 10%"
         />
-        <h1 className='absolute bottom-0 p-3 flex flex-grow w-full flex-shrink bg-gradient-to-t from-zinc-900'>
+        <h1 className='absolute bottom-0 inset-x-0 flex px-3 justify-center md:justify-start bg-gradient-to-t from-zinc-900'>
           The Wanderers Nook
         </h1>
       </div>
-      <div className='w-full px-3 pt-3'>
-        <p className='w-full'>Welcome to the Nook! Here you'll find walks and adventures around Greater Manchester. </p>
+      <div className='w-full px-3 md:px-5 lg:px-3 pt-3'>
+        <p className='w-full'>Welcome to the Nook! Here you'll find walks and adventures around Greater Manchester.</p>
         <p className='w-full'>I hope exploring my site will help you explore the outdoors.</p>
       </div>
-      <div className='w-full p-3'>
+      <div className='w-full px-3 md:px-5 lg:px-3'>
         <h2 className=''>Hikes</h2>
         <p className='w-full'>Are you a veteran hiker looking for new places to wander? A new hiker, who's always wanted to go outside more but never quite known where to go? You'll find a walk to suit you here.</p>
-        <p className='w-full'>All my walks are accessible by public transport, and some start in the centre. This way, you can always have a drink or two at the end. After all, what is a hike, but a nice walk to a good pub?</p>
+        <p className='w-full'>All my walks are accessible by public transport, with some starting in the centre. This way, there's no fuel price, or finding parking space on summer days.</p>
         <p className='w-full'>I recommend searching by area. You can choose something close or far and see what scenery you might see.</p>
-        <div className='inline-flex w-full justify-around md:justify-end py-4'>
-          <Link to='/routes' className='inline-flex px-2 py-1 md:mr-4 items-center rounded-full bg-red'>
+        <div className='flex flex-wrap justify-around md:justify-start md:flex-row-reverse py-4'>
+          <Link to='/routes' className='flex px-2 py-1 mb-2 md:mb-0 md:mr-2 items-center rounded-full bg-red'>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" className="flex size-6 stroke-white">
               <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
             </svg>
             <span className='flex text-left ml-2 mr-1 text-white text-base md:text-lg'>Explore all hikes</span>
           </Link>
-          <Link to='/locations' className='inline-flex px-2 py-1 items-center rounded-full bg-red'>
+          <Link to='/locations' className='flex justify-self-center px-2 py-1 md:mr-2  items-center rounded-full bg-red'>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" className="flex size-6 stroke-white">
               <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
             </svg>
@@ -47,11 +47,11 @@ const IndexPage = () => {
           <h3 className=''>Highlighted hikes</h3>
         </div>
       </div>
-      <div className='w-full p-3'>
+      <div className='w-full px-3 md:px-5 lg:px-3'>
         <h2 className=''>Articles</h2>
         <p className='w-full'>I have written various articles on the hikes I have done. These can be great for finding walks when you don't know what to look for.</p>
         <p className='w-full'>Some of these detail planning hikes. I plan to do some on gear and what to bring in future.</p>
-        <div className='inline-flex w-full justify-around md:justify-end pt-2'>
+        <div className='inline-flex w-full justify-around md:justify-end py-4'>
           <button className='inline-flex px-2 py-1 mr-2 items-center rounded-full bg-red'>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" className="flex size-6 stroke-white">
               <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
@@ -59,7 +59,7 @@ const IndexPage = () => {
             <span className='flex text-left ml-2 mr-1 text-white text-base md:text-lg'>Explore all articles</span>
           </button>
         </div>
-        <div className='w-full'>
+        <div className='w-full mt-1'>
           <h3 className=''>Highlighted articles</h3>
         </div>
       </div>
