50a038ac83cb4f004363bc85153d51fd9aabda3a
Nathaniel <nstanton1@sheffield.ac.uk>
2024-08-07 00:11:56 +0100

    Some minor updates.

--- src/pages/index.js
+++ src/pages/index.js
@@ -58,12 +58,12 @@ const IndexPage = () => {
           <p className='w-full'>I have written various articles on the hikes I have done. These can be great for finding walks when you don't know what to look for.</p>
           <p className='w-full'>Some of these detail planning hikes. I plan to do some on gear and what to bring in future.</p>
           <div className='inline-flex w-full justify-around md:justify-end py-4'>
-            <button className='inline-flex px-2 py-1 mr-2 items-center rounded-full bg-accent-red'>
+            <Link to='/blog' className='inline-flex px-2 py-1 mr-2 items-center rounded-full bg-accent-red'>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" className="flex size-6 stroke-white">
                 <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
               </svg>
               <span className='flex text-left ml-2 mr-1 text-white text-base md:text-lg'>Explore all articles</span>
-            </button>
+            </Link>
           </div>
           <div className='w-full mt-1'>
             <h3 className=''>Highlighted articles</h3>
