3880afbfa4052e4e8f833e29a2db5b142075caf4
Nathaniel <nstanton1@sheffield.ac.uk>
2024-10-12 00:41:22 +0100

    Added all locations to view with popup links.

--- src/pages/locations/index.js
+++ src/pages/locations/index.js
@@ -2,31 +2,59 @@
 import * as React from 'react'
 import {Layout, Padding} from '../../components/layout'
 import Seo from '../../components/seo'
-import { graphql } from 'gatsby'
+import { graphql, Link } from 'gatsby'
 import { LocationCard } from '../../components/location-card'
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup, FeatureGroup } from 'react-leaflet'
 const {getSlug} = require('../../components/helperFunctions')
 
 // Define components
 const LocationPage = ({data}) =>{
     const {nodes} = data.allMarkdownRemark;
+
+    var left;
+    var top;
+    var right;
+    var bottom;
+    nodes.forEach((location) => {
+      if (bottom == null || location.frontmatter.location.latitude < bottom) {
+        bottom = location.frontmatter.location.latitude
+      }
+      if (top == null || location.frontmatter.location.latitude > top) {
+        top = location.frontmatter.location.latitude
+      }
+      if (left == null || location.frontmatter.location.longitude < left) {
+        left = location.frontmatter.location.longitude
+      }
+      if (right == null || location.frontmatter.location.longitude > right) {
+        right = location.frontmatter.location.longitude
+      }
+    });
+
     return (
         <Layout>
         <Padding>
           <div className='w-full'>
             <h1 className='mt-8 w-full'>Locations to explore</h1>
-            <p>Explore the various areas of Greater Manchester you can visit easily. Select whichever looks interesting for you to view the routes I have found nearby.</p>
+            <p>Explore the various areas of Greater Manchester you can visit easily. Select whichever looks interesting for you to view the routes I have found nearby. {top}</p>
 
-            <MapContainer style={{ height: '400px' }} center={[53.47855858619753, -2.243319865628266]} zoom={13} scrollWheelZoom={true}>
+            <MapContainer style={{ height: '500px' }} scrollWheelZoom={true} bounds={[[bottom,left],[top,right]]} boundsOptions={{padding: [50, 50]}}>
               <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
               />
-              <Marker position={[53.47855858619753, -2.243319865628266]}>
-                <Popup>
-                  A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-              </Marker>
+              <FeatureGroup>
+                {
+                  nodes.map(location => (
+                    <Marker position={[location.frontmatter.location.latitude, location.frontmatter.location.longitude]}>
+                      <Popup>
+                        {location.frontmatter.title} 
+                        <br/>
+                        <Link to={"/locations/" + getSlug(location.fileAbsolutePath)} className='underline'>Click here to view</Link>
+                      </Popup>
+                    </Marker>
+                  ))
+                }
+              </FeatureGroup>
             </MapContainer>
 
             <div className='w-full flex flex-wrap justify-around lg:justify-between'>
@@ -80,6 +108,10 @@ export const query = graphql`
           travelTime
           type
           excerpt
+          location {
+            latitude
+            longitude
+          }
           heroImage {
             childImageSharp {
                 gatsbyImageData(aspectRatio: 1.778, placeholder: DOMINANT_COLOR, width: 300)
