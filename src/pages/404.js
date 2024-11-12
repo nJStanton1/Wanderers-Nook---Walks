81cb1bd0d607d8112f14bc5635d928ba2922e3aa
Nathaniel <nstanton1@sheffield.ac.uk>
2024-11-12 01:26:33 +0000

    404 page updated.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,46 +1,14 @@
 import * as React from "react"
-import { Link } from "gatsby"
-
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
+import { Layout } from "../components/layout"
+import { InternalButton } from "../components/button"
 
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
+    <Layout>
+      <h1 className="mt-10">Oops. The dreaded 404.</h1>
+      <p>Sorry, this page doesn't exist. The button below takes you back home.</p>
+      <InternalButton text='Go home' linkTo=""/>
+    </Layout>
   )
 }
 
