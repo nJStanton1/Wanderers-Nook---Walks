7c4f602f081ce59743634a2715f324c2fdb1242b
Nathaniel <nstanton1@sheffield.ac.uk>
2026-01-04 20:27:53 +0000

    added accent text to css and made some design changes to anchor buttons.

--- src/components/anchor-menu.js
+++ src/components/anchor-menu.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 export const AnchorButton = ({text, link}) => (
-    <Link to={link} className={`flex grow px-3 py-1 justify-center text-accent-walks font-medium text-base md:text-lg rounded-full bg-slate-800/10 border-2 accent-border hover:text-white hover:accent-border-dark hover:accent-dark-background`}>
+    <Link to={link} className={`flex grow px-3 py-1 justify-center text-white font-medium text-base md:text-lg rounded-full bg-slate-800/10 border-2 accent-border hover:text-white hover:accent-border-dark hover:accent-dark-background`}>
         {text}
     </Link>
 )
